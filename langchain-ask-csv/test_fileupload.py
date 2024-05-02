7e41591931bac5fa720ea0fac42cfe2ff855602f
H9b1b9h <168689553+H9b1b9h@users.noreply.github.com>
2024-05-02 13:17:09 +1000

    Add files via upload

---
+++ langchain-ask-csv/test_fileupload.py
@@ -0,0 +1,26 @@
+from langchain_experimental.agents import create_csv_agent
+from langchain_community.llms import OpenAI
+from dotenv import load_dotenv
+import os
+import streamlit as st
+
+
+def main():
+    load_dotenv()
+
+    # Load the OpenAI API key from the environment variable
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV 📈")
+
+    csv_file = st.file_uploader("Upload a CSV file", type="csv")
+
+    
+
+if __name__ == "__main__":
+    main()
